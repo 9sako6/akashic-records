9ae4b9a4d8b120276932e3a845d0783a2fcf1bd1
9sako6 <31821663+9sako6@users.noreply.github.com>
2020-07-04 16:43:15 +0900

    refactoring

--- src/storage/Storage.ts
+++ src/storage/Storage.ts
@@ -1,1 +1,11 @@
-export class Storage {}
+import { BufferPool } from './BufferPool';
+import { DiskManager } from './DiskManager';
+
+export class Storage {
+  bufferPool: BufferPool;
+  diskManager: DiskManager;
+  constructor() {
+    this.bufferPool = new BufferPool();
+    this.diskManager = new DiskManager();
+  }
+}
